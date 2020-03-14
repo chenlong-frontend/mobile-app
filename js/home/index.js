25b809b358dc8c42632da1525538c3d25dab28a8
1016482011 <2956957982@qq.com>
2020-03-14 14:16:31 +0800

    添加注释

--- js/home/index.js
+++ js/home/index.js
@@ -69,10 +69,12 @@ Home.prototype = {
 				.data('taskInsCode', data[i].taskInsCode);
 			ulCopy.append(liCopy);
 		}
+		// 监听完成点击
 		ulCopy.on('click', '.jq-complete', function(e) {
 			var taskInsCode = $(this).parent().parent().data("taskInsCode")
 			console.log(taskInsCode)
 		});
+		// 监听星标点击
 		ulCopy.on('click', '.jq-star', function(e) {
 			var taskInsCode = $(this).parent().parent().data("taskInsCode")
 			console.log(taskInsCode)
