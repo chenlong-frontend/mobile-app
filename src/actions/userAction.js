34daaf7ca1200d725d338a8ddb75fae59e340a30
1016482011 <2956957982@qq.com>
2020-06-13 19:39:38 +0800

    添加用户信息请求

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -1,16 +1,20 @@
 import {
-  USER_SET_INFO, USER_SET_LIST
+  USER_SET_INFO,
+  USER_SET_LIST
 } from '../constants/redux'
-import {getUserByToken, getUsers} from '../api/user'
+import {
+  getUserByToken,
+  getUsers
+} from '../api/user'
 
-export function setUserInfoAction (info) {
+export function setUserInfoAction(info) {
   return {
     type: USER_SET_INFO,
     data: info
   }
 }
 
-export function setUserListAction (list) {
+export function setUserListAction(list) {
   return {
     type: USER_SET_LIST,
     data: list
@@ -18,7 +22,7 @@ export function setUserListAction (list) {
 }
 
 // 请求用户列表
-export function requestGetUsersAction () {
+export function requestGetUsersAction() {
   return async (dispatch) => {
     const data = await getUsers();
     dispatch(setUserListAction(data));
@@ -26,9 +30,10 @@ export function requestGetUsersAction () {
 };
 
 // 请求用户信息
-export function requestUserInfoAction () {
+export function requestUserInfoAction() {
   return async (dispatch) => {
     const data = await getUserByToken();
     dispatch(setUserInfoAction(data));
+    return data;
   }
 };
